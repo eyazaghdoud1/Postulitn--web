5492ad82f07c893030154a6c59cfe05fe7b9aaf3
eyazaghdoud1 <eya.zaghdoud@esprit.tn>
2023-04-25 21:53:01 +0200

    sms api

--- src/Controller/EntretiensController.php
+++ src/Controller/EntretiensController.php
@@ -13,8 +13,8 @@ use App\Repository\OffreRepository;
 use App\Entity\Entretiens;
 use App\Form\EntretiensType;
 use App\Repository\UtilisateurRepository;
+use Twilio\Rest\Client;
 
-use function PHPSTORM_META\type;
 
 class EntretiensController extends AbstractController
 {
@@ -31,16 +31,16 @@ class EntretiensController extends AbstractController
      * read entretiens method
      */
     #[Route('/entretiens', name: 'readEntretiens')]
-    public function readEntretiens(EntretiensRepository $Rep, UtilisateurRepository $userRepo):Response
+    public function readEntretiens(EntretiensRepository $Rep, UtilisateurRepository $userRepo): Response
     {
-       // $list = $Rep->findAll();
-       $list = $Rep->findByRecruteur(69);
-       $count = $Rep->numberOfEntretiensPerRecruteur(69);
-        return $this->render('entretiens/readEntretiens.html.twig', ['list' => $list, 
-        'count'=>$count,
-        'recruteur'=> $userRepo->find(69)
+        // $list = $Rep->findAll();
+        $list = $Rep->findByRecruteur(69);
+        $count = $Rep->numberOfEntretiensPerRecruteur(69);
+        return $this->render('entretiens/readEntretiens.html.twig', [
+            'list' => $list,
+            'count' => $count,
+            'recruteur' => $userRepo->find(69)
         ]);
-
     }
 
     /**
@@ -48,32 +48,32 @@ class EntretiensController extends AbstractController
      * read entretiens method for candidat
      */
     #[Route('/entretiensCandidat', name: 'entretiensCandidat')]
-    public function readEntretiensCandidat(EntretiensRepository $Rep, UtilisateurRepository $userRepo):Response
+    public function readEntretiensCandidat(EntretiensRepository $Rep, UtilisateurRepository $userRepo): Response
     {
-       // $list = $Rep->findAll();
-       $list = $Rep->findByCandidat(68);
-       $count = $Rep->numberOfEntretiensPerCandidat(68);
-        return $this->render('entretiens/readEntretiensCandidat.html.twig', ['list' => $list, 
-        'count'=>$count,
-        'candidat'=> $userRepo->find(68)
+        // $list = $Rep->findAll();
+        $list = $Rep->findByCandidat(68);
+        $count = $Rep->numberOfEntretiensPerCandidat(68);
+        return $this->render('entretiens/readEntretiensCandidat.html.twig', [
+            'list' => $list,
+            'count' => $count,
+            'candidat' => $userRepo->find(68)
         ]);
-
     }
 
-     /**
+    /**
      * 
      * read one entretien method
      */
     #[Route('/entretien/{id}', name: 'readEntretien')]
-    public function readEntretien(EntretiensRepository $Rep, $id):Response
+    public function readEntretien(EntretiensRepository $Rep, $id): Response
     {
         $entretien = $Rep->find($id);
-        return $this->render('entretiens/readEntretiens.html.twig', ['e' => $entretien
+        return $this->render('entretiens/readEntretiens.html.twig', [
+            'e' => $entretien
         ]);
-
     }
 
-     /**
+    /**
      * 
      * add entretien method
      */
@@ -82,26 +82,45 @@ class EntretiensController extends AbstractController
     public function addEntretien(ManagerRegistry $doctrine, Request $request, CandidaturesRepository $candRepo, $id): Response
     {
         $entretien = new Entretiens();
-       
+
         //$entretien->setIdcandidature($candRepo->find(29));
         $entretien->setIdcandidature($candRepo->find($id));
         $form = $this->createForm(EntretiensType::class, $entretien);
-        $form->handleRequest($request); 
-       
-        if ($form->isSubmitted() && $form->isValid()) {
-            //formatting the time to only save the hours and minutes
-            $entretien->setHeure(substr($form->get('heure')->getData(),0,5));
-            
-            if ($form->get('type')->getData() == "En présentiel")
-             { $candRepo->find($id)->setEtat("EntretienPres");
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+           
+            //formatting the time to only save the hours and minutes
+            $entretien->setHeure(substr($form->get('heure')->getData(), 0, 5));
+
+            if ($form->get('type')->getData() == "Présentiel") {
+                $candRepo->find($id)->setEtat("EntretienPres");
             } else {
                 $candRepo->find($id)->setEtat("EntretienTel");
-               
             }
             $em = $doctrine->getManager();
-            $em->persist($entretien); 
-            $em->flush(); 
+            $em->persist($entretien);
+            $em->flush();
+
+            /* sending a message to "candidat" once "recruteur" adds a new meeting  */
+            /* using twilio api */
+            $account_sid = $this->getParameter('twilio_account_sid');
+            $auth_token =  $this->getParameter('twilio_auth_token');
+            $twilio_phone_number =  $this->getParameter('twilio_number');
+            $receiver_phone_number = '+216' . $entretien->getIdcandidature()->getIdcandidat()->getTel();
+            
+
+            $client = new Client($account_sid, $auth_token);
+
+            $client->messages->create(
+                $receiver_phone_number,
+                array(
+                    "from" => $twilio_phone_number,
+                    "body" => "+1"
+                    //"body" => "Un entretien pour votre candidature au poste " . $entretien->getIdcandidature()->getIdoffre()->getPoste() . "a été planifié."
+                )
+            );
+
             return $this->redirectToRoute('readEntretiens');
         } else
             return $this->render('entretiens/add.html.twig', ['form' => $form->createView()]);
@@ -111,33 +130,51 @@ class EntretiensController extends AbstractController
      * 
      * update entretien method
      */
-    
+
     #[Route('/updateEntretien/{id}', name: 'updateEntretien')]
     public function updateEntretien(ManagerRegistry $doctrine, Request $request, $id, EntretiensRepository $repo): Response
     {
 
         $entretien = $repo->find($id);
 
-        $entretien->setHeure($entretien->getHeure().':00');
+        $entretien->setHeure($entretien->getHeure() . ':00');
         $entretien->setLieu("none");
         $form = $this->createForm(EntretiensType::class, $entretien);
         //$form->add('Modifier', SubmitType::class);
 
-        
+
         $form->handleRequest($request);
-        
+
         if ($form->isSubmitted() && $form->isValid()) {
             //formatting the time to only save the hours and minutes
-            $entretien->setHeure(substr($form->get('heure')->getData(),0,5));
-            
+            $entretien->setHeure(substr($form->get('heure')->getData(), 0, 5));
+
             $em = $doctrine->getManager();
-            $em->persist($entretien); 
+            $em->persist($entretien);
             $em->flush();
 
+            /* sending a message to "candidat" once "recruteur" updates a meeting's info  */
+            
+            /*$account_sid = $this->getParameter('twilio_account_sid');
+            $auth_token =  $this->getParameter('twilio_auth_token');
+            $twilio_phone_number =  $this->getParameter('twilio_number');
+            $receiver_phone_number = '+21692314270';
+
+            $client = new Client($account_sid, $auth_token);
+
+            $client->messages->create(
+                $receiver_phone_number,
+                array(
+                    "from" => $twilio_phone_number,
+                    "body" => "+1"
+                    //"body" => "Un entretien pour le poste " . $entretien->getIdcandidature()->getIdoffre()->getPoste() . " a été modifié."
+                )
+            );
+            */
             return $this->redirectToRoute('readEntretiens');
         }
 
-        return $this->render('entretiens/update.html.twig', ['form' => $form->createView(), 'e'=>$entretien]);
+        return $this->render('entretiens/update.html.twig', ['form' => $form->createView(), 'e' => $entretien]);
     }
 
     /**
@@ -148,11 +185,29 @@ class EntretiensController extends AbstractController
     public function deleteEntretien(EntretiensRepository $repo, ManagerRegistry $doctrine, $id): Response
     {
 
-        $objet=$repo->find($id);
-        $em=$doctrine->getManager();
+        $objet = $repo->find($id);
+        $em = $doctrine->getManager();
         $em->remove($objet);
         $em->flush();
-        return $this->redirectToRoute('readEntretiens');
+         /* sending a message to "candidat" once "recruteur" deletes a meeting  */
+            
+            /*$account_sid = $this->getParameter('twilio_account_sid');
+            $auth_token =  $this->getParameter('twilio_auth_token');
+            $twilio_phone_number =  $this->getParameter('twilio_number');
+            $receiver_phone_number = '+21692314270';
 
+            $client = new Client($account_sid, $auth_token);
+
+            $client->messages->create(
+                $receiver_phone_number,
+                array(
+                    "from" => $twilio_phone_number,
+                    "body" => "-1"
+                    //"body" => " * L'entretien du " + $entretien->getDate() . 
+                     " pour votre candidature au poste " . $entretien->getIdcandidature()->getIdoffre()->getPoste() . " a été annulé."
+                )
+            );
+            */
+        return $this->redirectToRoute('readEntretiens');
     }
 }
